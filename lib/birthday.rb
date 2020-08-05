5f8d928a3b8155686965dfb854ecbe3b8d619720
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-05 19:08:33 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,6 +7,6 @@ birthday_kids = {
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
-    puts "Happy birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy birthday #{kids_name}! You are now #{age} years old!\n"
   end
 end
